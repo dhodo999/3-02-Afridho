e675fdbfc6c55c1ce5466795635393e23ad10e32
dhodo999 <dashdho2@gmail.com>
2022-02-21 09:18:43 +0700

    modul 5

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,24 +1,32 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-    <div class="container">
-      <a class="navbar-brand" href="#">Afridho</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+  <div class="container">
+  <a class="navbar-brand" href="/">Afridho</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+      <li class="nav-item">
+          <a class="nav-link {{ ($title === 'Home') ? 'active' : ''}}"aria-current="page" href="/">Home</a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link {{ ($title === 'About') ? 'active' : ''}}" href="/about">About</a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link {{ ($title === 'Gallery') ? 'active' : ''}}" href="/gallery">Gallery</a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link {{ ($title === 'Contact') ? 'active' : ''}}" href="/contacts">Contact</a>
+      </li>
+      </ul>
+      <ul class="navbar-nav ms-auto">
           <li class="nav-item">
-            <a class="nav-link {{($title === 'Home') ? 'active' : ''}}" aria-current="page" href="/">Home</a>
+              <a class="nav-link" href="{{route('login')}}">Login</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link {{($title === 'About') ? 'active' : ''}}" href="/about">About</a>
+              <a class="nav-link" href="{{route('register')}}">Register</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link {{($title === 'Gallery') ? 'active' : ''}}" href="/gallery">Gallery</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{($title === 'Contacts') ? 'active' : ''}}" href="/contacts">Contact Us</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+      </ul>
+  </div>
+  </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home', [
-        "title" => "Home"
+    return view('index', [
+        "title" => "Beranda"
     ]);
 });
 
@@ -38,5 +38,8 @@ Route::get('/gallery', function () {
 Route::resource('/contacts', ContactController::class);
 
 
+Auth::routes();
 
-
+Route::group(['middleware' => ['auth']], function () {
+    route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
